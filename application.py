299a7c4969c82a19b175938bc8014de95308cca7
Lefevre <nlefevre650@headmind.com>
2024-08-22 17:07:44 +0200

    Update README & application

--- application.py
+++ application.py
@@ -11,8 +11,13 @@ from flask import Flask, render_template, request, redirect, url_for, abort, \
 from pathlib import Path
 import sys
 
+#Init est utilisé pour l'ajout de coleur dans le code
 init()
+
+#Permet de définir l'application Flask
 app = Flask(__name__)
+
+#Permet d'exécuter le fichie HTML
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -20,7 +25,10 @@ def index():
 #Crée le /upload du HTML et permet de sélectionner un fichier et de l'envoyer
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
+
     if request.method == 'POST':
+
+        #Permet la récupération du fcihier depuis l'HTML
         if 'file' not in request.files:
             return f'{Fore.RED}[-] No file part{Fore.RESET}'
         file = request.files['file']
@@ -28,25 +36,29 @@ def upload():
             return f'{Fore.RED}[-] No selected file{Fore.RESET}'
         
         file_name = file.filename
+        #Check si le fichier entré est bon (test pour répondre à CodeQL)
         if ".." in file_name or "/" in file_name or "\\" in file_name:
             raise ValueError(f"{Fore.RED}[-] Invalid filename : {file.filename}{Fore.RESET}")
-            
-        file_content = process_file(file_name)
-        print(f"{Fore.GREEN}[+] file uploded !{Fore.RESET}")
+        else:
+            file_content = process_file(file_name)
+            print(f"{Fore.GREEN}[+] file uploded !{Fore.RESET}")
     return render_template('index.html', file_content=file_content)
 
-
+#Utilisation de la fonction vulnérable selon le POC de la CVE-2020-1747
 def process_file(file_name):
     """Lit le fichier texte et retourne son contenu. Pour d'autres types de fichiers, ajustez le traitement."""
+    #Chargement du fichier YAML
     if ".yaml" in file_name:
         with open(file_name,'rb') as f:
             content = f.read()
+        #Exécution de la fonction vulnérable
         data = yaml.load(content, Loader=yaml.FullLoader) # Using vulnerable FullLoader
 
         return data
     else:
         return f"{Fore.RED}[-] Uploaded file is not a YAML file.{Fore.RESET}"
-    
+
+#Permet de générer le fichier requirements.txt
 def gen_reqtxt(directory):
 
     #Exécute pipreqs pour récupérer le nom des libs à mettre dans le requirements.txt
@@ -84,6 +96,7 @@ def gen_reqtxt(directory):
 
 
 if __name__ == '__main__':
+    #Récupère le path de l'application
     directory = f'{Path(__file__).parent}'
     gen_reqtxt(directory)
     app.run(debug=False)
